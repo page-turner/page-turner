6d3dbda6e255f453ecafea8148f41e5be827a2d5
joshua-8 <joshua_phelps@brown.edu>
2021-10-26 22:19:12 -0400

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+    pinMode(2, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    digitalWrite(2, HIGH);
+    delay(100);
+    digitalWrite(2, LOW);
+    delay(100);
 }
